61903dc04a120a6677140cf588356ce92fbb484e
NOTECOLT <6955795+NOTECOLT@users.noreply.github.com>
2026-08-27 23:12:49 +0800

    add zoom in animation

--- scripts/ui/GameCamera.cs
+++ scripts/ui/GameCamera.cs
@@ -1,7 +1,9 @@
 using Godot;
 using System;
-
 public partial class GameCamera : Node2D {
+	[Export]
+	public float zoomSpeed = 0.2f;
+
 	[Export]
 	public float unZoomedValue = 1;
 
@@ -20,6 +22,8 @@ public partial class GameCamera : Node2D {
 
 	private Vector2 _lastMousePos;
 
+	private Vector2 _mousePosOnZoom;
+
 	private AnimationPlayer _animPlayer;
 
 	public override void _Ready() {
@@ -28,17 +32,49 @@ public partial class GameCamera : Node2D {
 
 		gameManager.SetZoom += OnZoomToggled;
 		gameManager.BillWrong += OnBillWrong;
+
+		_camera.Zoom = Vector2.One * unZoomedValue;
 	}
 
 	public override void _Process(double delta) {
-		// Camera Movement when zoomed
+		
 		if (_isZoomed) {
+			// Camera zoom in;
+			if (_camera.Zoom.X < zoomedValue) {
+				_camera.Zoom += Vector2.One * zoomSpeed * (float)delta;
+
+				if (_camera.Zoom.X > zoomedValue) _camera.Zoom = Vector2.One * zoomedValue;
+				
+
+				// Have Camera move to the where the mouse was on clicking zoom
+				//		at the same speed at which the camera is zooming
+				// Distance between Camera & initial Mouse * the percentage the camera zoomed within the last frame
+				Position = Position.MoveToward(_mousePosOnZoom,	
+					Position.DistanceTo(_mousePosOnZoom) * (zoomSpeed / (zoomedValue - _camera.Zoom.X)) * (float)delta);
+			}
+
+			
+			// Camera Movement when zoomed
 			Vector2 currentMousePos = GetLocalMousePosition();
 			Vector2 mousePosDiff = _lastMousePos - currentMousePos;
 
 			Position -= mousePosDiff * 0.5f;
 
 			_lastMousePos = currentMousePos;
+
+		} else {
+			// Camera zoom out;
+			if (_camera.Zoom.X > unZoomedValue) {
+				_camera.Zoom -= Vector2.One * zoomSpeed * (float)delta;
+
+				if (_camera.Zoom.X < unZoomedValue) _camera.Zoom = Vector2.One * unZoomedValue;
+
+				// Have Camera move to unZoomedPosition
+				//		at the same speed at which the camera is zooming
+				// Distance between Camera & initial Mouse * the percentage the camera zoomed within the last frame
+				Position = Position.MoveToward(unZoomedPos,	
+					Position.DistanceTo(unZoomedPos) * (zoomSpeed / (zoomedValue - _camera.Zoom.X)) * (float)delta);
+			}
 		}
 	}
 
@@ -46,12 +82,8 @@ public partial class GameCamera : Node2D {
 		_isZoomed = isZoomed;
 
 		if (_isZoomed) {
-			_camera.Zoom = new Vector2(zoomedValue, zoomedValue);
-			_lastMousePos = GetLocalMousePosition(); // Record of position of mouse on zoom for camera movement
-			Position = GetGlobalMousePosition();;
-		} else {
-			_camera.Zoom = new Vector2(unZoomedValue, unZoomedValue);
-			Position = unZoomedPos;
+			// Record of position of mouse on zoom for camera movement
+			_mousePosOnZoom = _lastMousePos = GetLocalMousePosition(); 
 		}
 	}
 
